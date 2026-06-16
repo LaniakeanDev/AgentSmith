768eeb8c07bcdf649c1cde42915ccfe7460b95d1
rzins <laniakean.dev@gmail.com>
2026-06-16 18:07:18 +0200

    sandbox basic CLI functional

--- src/sandbox/cli.py
+++ src/sandbox/cli.py
@@ -2,6 +2,7 @@ import fire
 from .config import SandboxConfig
 from .sandbox_launcher import launch_sandbox
 import json
+import sys
 
 
 class SandboxCLI:
@@ -29,7 +30,32 @@ class SandboxCLI:
 
 
 def main():
-    fire.Fire(SandboxCLI)
+    if len(sys.argv) == 1:
+        print("Enter your code below to have it executed in the sandbox")
+        print("Press Ctrl+D when done")
+        print("-" * 50)
+        lines = []
+        try:
+            while True:
+                try:
+                    line = input()
+                    lines.append(line)
+                except EOFError:
+                    break
+        except KeyboardInterrupt:
+            print("\nExiting...")
+            return
+        if not lines:
+            print("No code entered. Exiting.")
+            return
+        code = '\n'.join(lines)
+        print("\n" + "=" * 50)
+        print(" " * 18 + "Executing code...")
+        print("=" * 50)
+        cli = SandboxCLI()
+        cli.execute(code)
+    else:
+        fire.Fire(SandboxCLI)
 
 
 if __name__ == "__main__":
